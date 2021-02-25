b6094c17678a3bcd0d64c46dd6b438f3f74f5c34
Vendera <vendera.hadi@gmail.com>
2021-02-25 14:50:19 +0700

    fix zipcode model

--- lib/prism/models/zipcode.rb
+++ lib/prism/models/zipcode.rb
@@ -1,28 +1,28 @@
 module Prism
   class Zipcode < PrismModel
+    acts_as_paranoid
 
-  acts_as_paranoid
+    belongs_to :district, optional: true
 
-  belongs_to :district, optional: true
+    scope :by_query, lambda { |query|
+      return where(nil) if query.blank?
 
-  scope :by_query, lambda { |query|
-    return where(nil) if query.blank?
+      where(
+        'zipcode ILIKE :query OR sub_district_name ILIKE :query OR
+        district_name ILIKE :query OR city_name ILIKE :query OR
+        province_name ILIKE :query',
+        query: "%#{query}%"
+      )
+    }
 
-    where(
-      'zipcode ILIKE :query OR sub_district_name ILIKE :query OR
-       district_name ILIKE :query OR city_name ILIKE :query OR
-       province_name ILIKE :query',
-      query: "%#{query}%"
-    )
-  }
+    def self.search(params = {})
+      params = {} if params.blank?
 
-  def self.search(params = {})
-    params = {} if params.blank?
+      by_query(params[:query])
+    end
 
-    by_query(params[:query])
-  end
-
-  def full_address_name
-    "#{sub_district_name}, #{district_name}, #{city_name}, #{province_name}"
+    def full_address_name
+      "#{sub_district_name}, #{district_name}, #{city_name}, #{province_name}"
+    end
   end
 end
