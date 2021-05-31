7a31d7ac91c8e4dd22f0c7d99ceeac115f216cd3
Wawan Kurniawan <ones07@gmail.com>
2021-05-31 17:34:20 +0700

    remove trailing white space

--- lib/prism/officers/order_website_status_officer.rb
+++ lib/prism/officers/order_website_status_officer.rb
@@ -16,9 +16,8 @@ module Prism
         time = Time.zone.now if status.to_s == website_status.to_s
 
         item_status = order_item.order_website_statuses.find_or_initialize_by(status: website_status)
-
         next if item_status.time.present?
-        
+
         item_status.time        = time if item_status.time.blank?
         item_status.is_complete = time.present?
         item_status.save!
