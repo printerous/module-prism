e7c09caab8de97a28f2fceff7107f80d65d2b9f3
ademiawansyah <arief.demiawansyah@gmail.com>
2021-07-08 22:03:01 +0700

    add logistic shipping courier service

---
+++ lib/prism/models/logistic_shipping_courier_service.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: logistic_shipping_courier_services
+#
+#  id                                       :bigint(8)        not null, primary key
+#  logistic_shipping_courier_id             :bigint(8)
+#  code                                     :string
+#  name                                     :string
+#  description                              :string
+#  deleted_at                               :datetime
+#  created_at                               :datetime         not null
+#  updated_at                               :datetime         not null
+#  logistic_shipping_courier_integration_id :bigint(8)
+#
+
+module Prism
+  class LogisticShippingCourierService < PrismModel
+    acts_as_paranoid
+  end
+end
