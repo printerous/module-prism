8832ae33104689622db35247b1846c9299cc1b17
ademiawansyah <arief.demiawansyah@gmail.com>
2021-07-13 21:37:09 +0700

    annotate

--- lib/prism/models/organization_product_shipping.rb
+++ lib/prism/models/organization_product_shipping.rb
@@ -2,11 +2,12 @@
 #
 # Table name: organization_product_shippings
 #
-#  id                              :bigint(8)        not null, primary key
-#  organization_shipping_config_id :bigint(8)
-#  deleted_at                      :datetime
-#  created_at                      :datetime         not null
-#  updated_at                      :datetime         not null
+#  id                               :bigint(8)        not null, primary key
+#  organization_shipping_config_id  :bigint(8)
+#  deleted_at                       :datetime
+#  created_at                       :datetime         not null
+#  updated_at                       :datetime         not null
+#  organization_partner_coverage_id :bigint(8)
 #
 
 module Prism
