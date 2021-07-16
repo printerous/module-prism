7e7810aeadedea4bb9144c56528fbe67f97efef5
ademiawansyah <arief.demiawansyah@gmail.com>
2021-07-16 10:02:31 +0700

    annotate

--- lib/prism/models/organization_partner_coverage.rb
+++ lib/prism/models/organization_partner_coverage.rb
@@ -10,6 +10,7 @@
 #  created_at                    :datetime         not null
 #  updated_at                    :datetime         not null
 #  integrations                  :jsonb
+#  hub_code                      :string
 #
 
 module Prism
